ef7e22819c673b40dc16e74fac68ee12fb2415bb
sko1521 <sudhir.kulat@springernature.com>
2020-05-03 12:31:10 +0530

    Updates

---
+++ precache-manifest.054523be68440c60765dc9c95fc3fef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8156f9013f76c60e4717754b38302e62",
+    "url": "/tictac/index.html"
+  },
+  {
+    "revision": "0375071abc81cc4b026c",
+    "url": "/tictac/static/css/main.a4715840.chunk.css"
+  },
+  {
+    "revision": "f62a4aa12d8162a59600",
+    "url": "/tictac/static/js/2.919d2037.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictac/static/js/2.919d2037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0375071abc81cc4b026c",
+    "url": "/tictac/static/js/main.b387410b.chunk.js"
+  },
+  {
+    "revision": "cae5d7cac1fe1da924a8",
+    "url": "/tictac/static/js/runtime-main.ebbd6e6e.js"
+  }
+]);
